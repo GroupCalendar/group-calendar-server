7ddf2df309349537580d67b1003a9f223fa3d4fa
Fatih Atasever <mail@fatihatasever.com>
2022-08-21 22:44:16 +0300

    *added logout button

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
 
   post "login", to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
+  get "logout", to: "sessions#destroy"
   get "login", to: "sessions#new"
 
   resources :passwords, only: [:create, :edit, :new, :update], param: :password_reset_token
