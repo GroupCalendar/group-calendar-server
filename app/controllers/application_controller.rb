c93680073ddf93bec07074619b70a51714025149
Fatih Atasever <mail@fatihatasever.com>
2022-08-24 11:48:56 +0300

    *layout updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   include Authentication
+
+  def is_current_controller(names)
+    names.include?(current_controller)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,4 +30,5 @@ Rails.application.routes.draw do
 
   resources :dashboard
   resources :groups
+  resources :documentation
 end
